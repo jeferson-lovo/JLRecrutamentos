c829fa033c92c3e12f40a8e55d5d33e4dc0898ea
jeferson-lovo <jefersonlovo2009@hotmail.com>
2025-08-22 21:11:23 -0400

    criando login parei na instalação do node

--- routes/web.php
+++ routes/web.php
@@ -1,54 +1,20 @@
 <?php
 
-use App\Http\Controllers\administrativo;
-use App\Http\Controllers\controleradm;
-use App\Http\Controllers\AperfeicoamentoController;
-use App\Http\Controllers\AreaController;
-use App\Http\Controllers\CidadeController;
-use App\Http\Controllers\CompetenciaController;
-use App\Http\Controllers\CurriculoController;
-use App\Http\Controllers\ExperienciaController;
-use App\Http\Controllers\FormacaoController;
-use App\Http\Controllers\MetodologiaController;
-use App\Http\Controllers\OportunidadeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-
-
-Route::get('/', [controleradm::class, 'homec'])->name('/');
-Route::get('/home', [controleradm::class, 'homec'])->name('home');
-Route::get('/cidades/{uf}', [CidadeController::class, 'getCidadesPorUf'])->name('cidades.por.uf'); //listar cidades apos selecionar UF
-Route::resource('/cidades', CidadeController::class); // controlador para as rotas cidade
-Route::resource('/oportunidades', OportunidadeController::class);
-Route::resource('/metodologias', MetodologiaController::class);
-Route::resource('/competencias', CompetenciaController::class);
-Route::resource('/areas', AreaController::class);
-Route::resource('/experiencias', ExperienciaController::class);
-Route::resource('/aperfeicoamentos', AperfeicoamentoController::class);
-Route::resource('/formacoes', FormacaoController::class)->parameters(['formacoes' => 'formacao']);
-Route::resource('/curriculos', CurriculoController::class);
-
-
-
-/*
-
-exemplos de rotas com parametros
-Route::get('/hello/{parametro}/{parametro1}', function($parametro, $parametro1) {
-    return "retorna parametros, $parametro $parametro1 ";
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/hello1/{parametro}/{parametro1?}', function($parametro, $parametro1=null) {
-    if (isset($parametro1))
-        return "retorna parametros, $parametro $parametro1 ";
-    return "parametro sem sobrenome";
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/hello2/{parametro}/{numero}', function($parametro, $numero) {
-    for($i=0; $i<$numero;$i++){
-        return "retorna parametros, $parametro $numero";
-    }
-
-})->where('parametro', '[A-Za-z]+')
-  ->where('numero', '[0-9]+');
-
-  */
+require __DIR__.'/auth.php';
